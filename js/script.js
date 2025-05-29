8455e0c8ec66806721254b46e3d5b5956e958829
Maksym Dramaretskyi <maksymdramaretskyi@gmail.com>
2025-05-29 12:47:11 +0200

    Homework completed #7

--- js/script.js
+++ js/script.js
@@ -1,14 +1,35 @@
 'use strict'
 
-let userName = prompt('Please, enter your name!');
-let userAnswer;
+/* The variable is declared as a const because the userName in this program will not change.
+This variable stores the username */
+const userName = prompt('What is your name?');
 
-if (userName){
-    userAnswer = confirm(`${userName }! Do you want to see the greeting?`)
+/*The variable is declared as a const because the userAges in this program will not change.
+ This variable stores the user's age.*/
+const userAge = Number(prompt('What is your age?'));
+
+/* The variable is declared as a const because the isUserVerification in this program will not change.
+This variable stores the true value that the user has verification.*/
+const isUserVerification = confirm('Do you agree to participate?');
+
+// Here I decided to add a condition and a variable to add a line when outputting to Alert
+let eventStatus;
+if (isUserVerification) {
+  eventStatus = "participation confirmed";
 } else {
-    userAnswer = confirm(`Do you want to see the greeting?`)
+  eventStatus = "participation denied";
 }
 
-if (userAnswer){
-    alert(`Welcome! ${userAnswer}!`)
-}
+const userId = BigInt("2343488459459934434");
+let bonusCode = null;
+let secondName;
+
+let messageToAlert = `Hi, ${userName} \nYour age is ${userAge} \nYour status is ${eventStatus}`;
+alert(messageToAlert);
+
+console.log(`Name: ${userName}, type - ${typeof userName}`);
+console.log(`Age: ${userAge}, type - ${typeof userAge}`);
+console.log(`Verification: ${isUserVerification}, type - ${typeof isUserVerification}`);
+console.log(`Id: ${userId}, type - ${typeof userId}`);
+console.log(`BonusCode: ${bonusCode} type - ${typeof bonusCode}`);
+console.log(`secondName: ${secondName} type - ${typeof secondName}`);
