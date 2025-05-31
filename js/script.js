934409c56034666562faf366adc4eb65c573915e
Maksym Dramaretskyi <maksymdramaretskyi@gmail.com>
2025-05-31 22:24:41 +0200

    HomeWork#11

--- js/script.js
+++ js/script.js
@@ -1,21 +1,42 @@
 'use strict'
 
-let objectUser = {
-  name: "Jack",
-  age: 18,
-  isOnline: true,
-  friends: ['Karl', 'Petty', 'Larry'],
-  settings: {
-    theme: 'dark',
-    notifications: false
+const userName = prompt('What is your name?');
+const userAge = Number(prompt('What is your age?'));
+const isUserNotifications = confirm('Do you want to receive notifications?');
+
+let objectSession = {
+  name: userName,
+  age: userAge,
+  notifications: isUserNotifications,
+  userId: 1234567890123456789n,
+  lastLogin: null,
+  nickname: undefined,
+  favoriteTech: ['HTML', 'CSS', 'REACT'],
+  settings:{
+    theme:'light',
+    autoLogin: false,
   }
-};
+}
 
-let firstFriendName = objectUser.friends[0];
-let notificationsEnabled =objectUser.settings.notifications;
+alert(`Hi ${userName}! Your ID:${objectSession.userId}`);
+console.log(objectSession);
 
-console.log(`User’s name: ${objectUser.name}`);
-console.log(`Number of friends: ${objectUser.friends.length}`);
-console.log(`Name of first friend: ${firstFriendName}`);
-console.log(`Theme is enabled: ${objectUser.settings.theme}`);
-console.log(`Сongratulations: ${notificationsEnabled}`);
+console.log(`Name-type:  ${typeof(objectSession.name)}`);
+
+console.log(`Age-type:  ${typeof(objectSession.age)}`);
+
+console.log(`Notifications-type:  ${typeof(objectSession.notifications)}`);
+
+console.log(`ID-type:  ${typeof(objectSession.userId)}`);
+
+console.log(`Login-type:  ${typeof(objectSession.lastLogin)}`);
+
+console.log(`NickName-type:  ${typeof(objectSession.nickname)}`);
+
+console.log(`favoriteTech-type:  ${typeof(objectSession.favoriteTech)}`);
+
+console.log(`settings-type:  ${typeof(objectSession.settings)}`);
+
+console.log(`settings.theme-type:  ${typeof(objectSession.settings.theme)}`);
+
+console.log(`settings.autoLogin-type:  ${typeof(objectSession.settings.autoLogin)}`);
