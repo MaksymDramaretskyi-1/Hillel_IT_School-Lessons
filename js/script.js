6ccab9d956ae66a488794fcaa615bf7c670af1a0
Maksym Dramaretskyi <maksymdramaretskyi@gmail.com>
2025-05-31 12:11:04 +0200

    HomeWork#10

--- js/script.js
+++ js/script.js
@@ -1,35 +1,21 @@
 'use strict'
 
-/* The variable is declared as a const because the userName in this program will not change.
-This variable stores the username */
-const userName = prompt('What is your name?');
+let objectUser = {
+  name: "Jack",
+  age: 18,
+  isOnline: true,
+  friends: ['Karl', 'Petty', 'Larry'],
+  settings: {
+    theme: 'dark',
+    notifications: false
+  }
+};
 
-/*The variable is declared as a const because the userAges in this program will not change.
- This variable stores the user's age.*/
-const userAge = Number(prompt('What is your age?'));
+let firstFriendName = objectUser.friends[0];
+let notificationsEnabled =objectUser.settings.notifications;
 
-/* The variable is declared as a const because the isUserVerification in this program will not change.
-This variable stores the true value that the user has verification.*/
-const isUserVerification = confirm('Do you agree to participate?');
-
-// Here I decided to add a condition and a variable to add a line when outputting to Alert
-let eventStatus;
-if (isUserVerification) {
-  eventStatus = "participation confirmed";
-} else {
-  eventStatus = "participation denied";
-}
-
-const userId = BigInt("2343488459459934434");
-let bonusCode = null;
-let secondName;
-
-let messageToAlert = `Hi, ${userName} \nYour age is ${userAge} \nYour status is ${eventStatus}`;
-alert(messageToAlert);
-
-console.log(`Name: ${userName}, type - ${typeof userName}`);
-console.log(`Age: ${userAge}, type - ${typeof userAge}`);
-console.log(`Verification: ${isUserVerification}, type - ${typeof isUserVerification}`);
-console.log(`Id: ${userId}, type - ${typeof userId}`);
-console.log(`BonusCode: ${bonusCode} type - ${typeof bonusCode}`);
-console.log(`secondName: ${secondName} type - ${typeof secondName}`);
+console.log(`User’s name: ${objectUser.name}`);
+console.log(`Number of friends: ${objectUser.friends.length}`);
+console.log(`Name of first friend: ${firstFriendName}`);
+console.log(`Theme is enabled: ${objectUser.settings.theme}`);
+console.log(`Сongratulations: ${notificationsEnabled}`);
