fbf0807a1943ad33c0a2fc5a5165991a27834836
Maksym Dramaretskyi <maksymdramaretskyi@gmail.com>
2025-06-04 18:27:46 +0200

    start hw15

--- js/script.js
+++ js/script.js
@@ -1,12 +1,2 @@
 'use strict'
 
-// Get three numbers from the user (use + to round to a number)
-const enterFirstNumber = +prompt('Write the First number');
-const enterSecondNumber = +prompt('Write the Second number');
-const enterThirdNumber = +prompt('Write the Third number');
-
-// Calculate the sum and arithmetic mean
-const sum = enterFirstNumber + enterSecondNumber + enterThirdNumber;
-const result = sum / 3;
-
-alert(`${result} -  Arithmetic mean of the three numbers`);
