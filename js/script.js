7be08e085b6be6c3510d508a91f8fca9326e6003
Maksym Dramaretskyi <maksymdramaretskyi@gmail.com>
2025-06-14 00:17:31 +0200

    Done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
 'use strict'
 
-function greetUser(name) {
-  return `Hi, ${name}!`;
+const userEnterValue = +prompt("Enter value");
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789:';
+
+const generateKey = (length, characters)=> {
+
+  let resultRandom = '';
+
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    // Generate a random index from 0 to characters.length - 1
+    const randomIndexCharacters = Math.floor(Math.random() * charactersLength);
+    //Add a random symbol
+    resultRandom += characters[randomIndexCharacters];
+  }
+
+  return resultRandom;
 }
 
-console.log(greetUser("Max"));
+
+const key = generateKey(userEnterValue, characters);
+console.log(key);
