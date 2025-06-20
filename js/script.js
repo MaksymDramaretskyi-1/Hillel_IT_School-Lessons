0b89c2ec407775a133e87ebeacb9e637c8af4e44
Maksym Dramaretskyi <maksymdramaretskyi@gmail.com>
2025-06-20 13:35:32 +0200

    done

--- js/script.js
+++ js/script.js
@@ -1,14 +1,61 @@
 'use strict'
 
-const array = [1, 2, 3, 4, 5, 6, 7];
-
-function removeElement(array, item) {
-  const index = array.indexOf(item);
-  if (index !== -1) {
-    array.splice(index, 1);
-  }
+//1
+function averageOfNumbers(arr) {
+  const numbers = arr.filter(item => typeof item === 'number');
+  if (numbers.length === 0) return null;
+  const sum = numbers.reduce((acc, val) => acc + val, 0);
+  return sum / numbers.length;
 }
 
+const mixedArray = [1, "Maxim", 5, null, 10, true];
+console.log(averageOfNumbers(mixedArray));
 
-removeElement(array, 3);
-console.log(array);
+
+//2
+function doMath(x, znak, y) {
+  switch (znak) {
+    case '+': return x + y;
+    case '-': return x - y;
+    case '*': return x * y;
+    case '/': return y !== 0 ? x / y : 'Divided by 0';
+    case '%': return x % y;
+    case '^': return Math.pow(x, y);
+    default: return 'Unfaithful sign';
+  }
+}
+console.log(doMath(5, '+', 3));
+console.log(doMath(2, '^', 3));
+
+
+//3
+function fillArray() {
+  const rows = +prompt("Enter number of rows:");
+  const cols = +prompt("Enter the number of people:");
+  const result = [];
+
+  for (let i = 0; i < rows; i++) {
+    result[i] = [];
+    for (let j = 0; j < cols; j++) {
+      result[i][j] = prompt(`Enter value for [${i}][${j}]:`);
+    }
+  }
+  return result;
+}
+
+// const myArray = fillArray();
+// console.log(myArray);
+
+
+//4
+function removeChars(str, charsToRemove) {
+  let result = '';
+  for (let char of str) {
+    if (!charsToRemove.includes(char)) {
+      result += char;
+    }
+  }
+  return result;
+}
+
+console.log(removeChars("hello world", ['l', 'd'])); // "heo wor"
